f73e6848249df9e275c2b93bfe701992758c6084
Kaushik <63477668+cosecdelta@users.noreply.github.com>
2020-04-18 17:26:17 -0500

    Add files via upload

---
+++ quality.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 18 14:38:34 2020
+
+@author: kdutta01
+"""
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.io as scio
+from skimage.metrics import structural_similarity as ssim
+from skimage.metrics import peak_signal_noise_ratio as psnr
+from skimage.metrics import mean_squared_error as mse
+
+data_dir = "data"
+results_dir = "results"
+ssim_array = []
+psnr_array = []
+mse_array = []
+
+path_data = os.path.join(data_dir,"project_data.mat")
+original_image = scio.loadmat(path_data)["imgref"]
+plt.imshow(original_image, cmap = 'gray')
+
+datasets = [(os.path.join(results_dir,"reconstructionFullReconstruction.txt")),
+            (os.path.join(results_dir,"reconstruction270Reconstruction.txt")),
+            os.path.join(results_dir,"reconstruction90Reconstruction.txt")]
+
+for i in range(0,len(datasets)):
+    image_recon = np.loadtxt(datasets[i]);
+    plt.imshow(image_recon, cmap = 'gray')
+    ssim_array.append(ssim(original_image, image_recon, data_range = original_image.max() - original_image.min()))
+    psnr_array.append(psnr(original_image, image_recon, data_range = original_image.max() - original_image.min()))
+    mse_array.append(mse(original_image, image_recon))
+    
