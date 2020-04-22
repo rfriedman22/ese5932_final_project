321870a6ee1a95e540f089b580afcd15df5cee07
Kaushik <63477668+cosecdelta@users.noreply.github.com>
2020-04-21 23:45:00 -0500

    Add files via upload

--- quality.py
+++ quality.py
@@ -6,6 +6,7 @@ Created on Sat Apr 18 14:38:34 2020
 """
 
 import os
+import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.io as scio
@@ -15,21 +16,34 @@ from skimage.metrics import mean_squared_error as mse
 
 data_dir = "data"
 results_dir = "results"
+
 ssim_array = []
-psnr_array = []
 mse_array = []
+
+mse_array_roi = [];
+ssim_array_roi = [];
 
 path_data = os.path.join(data_dir,"project_data.mat")
 original_image = scio.loadmat(path_data)["imgref"]
-plt.imshow(original_image, cmap = 'gray')
+original_image_roi = original_image[175:220,100:140]
 
-datasets = [(os.path.join(results_dir,"reconstructionFullReconstruction.txt")),
-            (os.path.join(results_dir,"reconstruction270Reconstruction.txt")),
-            os.path.join(results_dir,"reconstruction90Reconstruction.txt")]
+datasets = [(os.path.join(results_dir,"sartFullReconstruction.txt")),
+            (os.path.join(results_dir,"sart270Reconstruction.txt")),
+            os.path.join(results_dir,"sart90Reconstruction.txt"),
+            os.path.join(results_dir,"tv_fista_full_image_beta_1e-3.txt")]
 
 for i in range(0,len(datasets)):
     image_recon = np.loadtxt(datasets[i]);
+    image_recon_roi = image_recon[175:220,100:140]
+    
+    # Global Image comparision Metrics
     ssim_array.append(ssim(original_image, image_recon, data_range = original_image.max() - original_image.min()))
-    psnr_array.append(psnr(original_image, image_recon, data_range = original_image.max() - original_image.min()))
     mse_array.append(mse(original_image, image_recon))
     
+    # ROI Defined comparision Metrics
+    ssim_array_roi.append(ssim(original_image_roi, image_recon_roi, data_range = original_image.max() - original_image.min()))
+    mse_array_roi.append(mse(original_image_roi, image_recon_roi))
+
+    
+plt.imshow(original_image_roi,cmap='gray')
+
